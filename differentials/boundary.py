59ec84f51b2c2b57569287ac3ebbb317b8d978a3
Wingfooted <Wingfooted3@gmail.com>
2024-09-24 17:21:13 +1000

    working typing for expressions

--- differentials/boundary.py
+++ differentials/boundary.py
@@ -21,19 +21,45 @@ class term:
         return lambda x: self.func(u)(x)
 
     def __eq__(self, other):
-        if isinstance((self, other), term):
+        # evaluated RHS - LHS
+        if isinstance(other, term) and isinstance(self, term):
+            # self works, problem term is other
             return lambda u: lambda x: other(u)(x) - self(u)(x)
-
-        else:
-            if isinstance(other, int) or isinstance(other, float):
-                self.__eq__(term(lambda u: lambda x: jax.array((other))), self)
-
-
+            return lambda u: lambda x: other(u)(x)
+            return lambda u: lambda x: self(u)(x)
+        elif not isinstance(other, term):
+            # turn other into a term
+            const_term = term(lambda u: lambda x: other)
+            return self.__eq__(const_term)
     def __add__(self, other):
+        corrected_other = self.convert_other(other)
+        return term(lambda u: lambda x: self(u)(x) + corrected_other(u)(x))
+
+    def __sub__(self, other):
+        corrected_other = self.convert_other(other)
+        return term(lambda u: lambda x: self(u)(x) - corrected_other(u)(x))
+
+    def __pow__(self, other):
         pass
 
+    def __mul__(self, other):
+        corrected_other = self.convert_other(other)
+        return term(lambda u: lambda x: self(u)(x) * corrected_other(u)(x))
+
+    def __truediv__(self, other):
+        corrected_other = self.convert_other(other)
+        print("true_div")
+        return term(lambda u: lambda x: self(u)(x) / corrected_other(u)(x))
+
+    def convert_other(self, other):
+        if isinstance(other, term):
+            return other
+        else:
+            const_term = term(lambda u: lambda x: other)
+            return const_term
 
 class derivative:
+    # function. term -> term
     def __init__(self, arg):
         self.arg = arg
 
@@ -46,12 +72,12 @@ class derivative:
 
         return term(lambda u: jax.grad(target(u), argnums=self.arg))
 
-class function:
-    def __init__(self):
-        pass
+
+class manual_function:
+    def __new__(self, operation):
+        return term(lambda u: lambda x: operation(x))
 
 
 dx = derivative(0)
 dt = derivative(1)
-sin = function()
-
+sin = manual_function(lambda x: jnp.sin(x))
--- differentials/test.py
+++ differentials/test.py
@@ -1,26 +1,48 @@
 import jax
 import jax.numpy as jnp
 
-from boundary import function, dx, dt
+from boundary import function, dx, dt, term
 
 # dx = lambda u: jax.grad(u, argnums=0)
 
 u = function()
-x = jnp.array((0.1, 0.1))
+x = jnp.array((1.0, 4.0))
 
 f = lambda x: jnp.sin(x[0]) * jnp.cos(x[1]) + jnp.exp(x[0])
 
-ux = dx(u)
-uxx = dx(ux)
-ut = dt(u)
+# term adding
+expr_add = u + dx(u) == dt(u) + dx(dx(u)) + dx(u)
+LHS = u + dx(u)
+RHS = dt(u) + dx(dx(u)) + dx(u)
+print(expr_add(f)(x))
+print(LHS(f)(x))
+print(RHS(f)(x))
 
-print(ut(f)(x))
-print(ux(f)(x))
-print(uxx(f)(x))
+# testing addition of numbers
+print("NEGATIVE DEALING ---------------")
+neg_expr = dx(u) - u
+cost_expr = dx(u) - 4
+print(dx(u)(f)(x))
+print("here", cost_expr(f)(x))
 
-expr = dt(u) == 4
-print(expr)
-print(expr(f)(x))
+# testing addition of numbers
+print("MULTIPLICATION DEALING ---------------")
+div_expr = dx(u)*dt(u)
+print(dx(u)(f)(x))
+print(dt(u)(f)(x))
+print(div_expr(f)(x))
+
+expr = u == 4
+expr1 = -4 == u
+print(type(expr(f)(x)), expr(f)(x))
+print(type(expr1(f)(x)), expr1(f)(x))
+
+# print(u(f)(x))
+# print(const(f)(x))
+# print(u(f)(x)+const(f)(x))
+
+# print(expr(f)(x))
+
 
 # manual diff
 
